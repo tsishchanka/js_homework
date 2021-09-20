739f5a7a14e1b3024e0c2b4e6d102f48e8690959
alena tsishchanka <e.tsish@gmail.com>
2021-09-20 14:16:24 +0300

    comments was deleted

--- poke.js
+++ poke.js
@@ -31,36 +31,6 @@ const getOnePokemonInfo = (namePokemon) => {
 }
 getOnePokemonInfo("charmeleon");
 
-
-/* const getPokemonInfo = (namePokemon) => {
-    getAllPokemons()
-        .then(pokemonsValue => {
-
-
-            return pokemonsValue.results.reduce((result, { name, url }) => {
-                if (name === namePokemon) {
-                    result = url;
-                }
-                console.log(result);
-                return result;
-            }, '');
-        })
-
-    .then((pokUrl) => {
-        console.log(pokUrl);
-        if (pokUrl === '') {
-            return console.log(`Покемон, по имени ${namePokemon} не найден`);;
-        }
-        try {
-            fetch(pokUrl).then(response => response.json()).then(value => { console.log(value) }).catch(error => console.log(error));
-        } catch (error) {
-            console.log(error) // if pokUrl = '', why this block catch doesn`t catch error, beacause we try to fetch ''
-        }
-    })
-}
-
-getPokemonInfo("charmeleon"); */
-
 //3
 const getAllPokemons = async() => {
     try {
@@ -93,21 +63,3 @@ const getArrayPokemonInfo = async() => {
 
 
 getArrayPokemonInfo()
-
-
-
-
-
-/* const getOnePokemonInfo = (namePokemon) => {
-    try {
-        getOnePokemon(namePokemon).then(pokemonInfo => {
-            if (pokemonInfo === undefined) {
-                return console.log(`Попробуйте найти другого покемона`)
-            }
-            console.log(pokemonInfo);
-        })
-    } catch (error) {
-        console.log(error)
-    }
-} */
-//getOnePokemonInfo("charmeleon");
